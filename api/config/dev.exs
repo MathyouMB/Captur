14a0ae2d47ae9d0d76c5757ca2719eebf7288000
MathyouMB <matthewmacraebovell@bell.net>
2020-06-07 20:41:03 -0400

    changed db config

--- api/config/dev.exs
+++ api/config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :captur, Captur.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "1234",
   database: "captur_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
