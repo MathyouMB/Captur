4fcc249c52a8d80f27ea2cb7985a48b71f681cf0
MathyouMB <matthewmacraebovell@bell.net>
2020-06-09 23:58:06 -0400

    init client

--- api/lib/captur/resolvers/game_resolver.ex
+++ api/lib/captur/resolvers/game_resolver.ex
@@ -4,4 +4,18 @@ defmodule Captur.GameResolver do
   def all(_args, _info) do
     {:ok, Games.list_games()}
   end
+
+  def create(args, _info) do
+    Games.create_game(args)
+  end
+
+  def update(%{id: id, game: game_params}, _info) do
+    Games.get_game!(id)
+    |> Games.update_game(game_params)
+  end
+
+  def delete(%{id: id}, _info) do
+    Games.get_game!(id)
+    |> Games.delete_game
+  end
 end
